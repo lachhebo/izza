e256fc3b5ab6c489302e4952dded35b1ba624b02
Ismaïl Lachheb <ismael.lachheb@protonmail.com>
2019-09-25 15:27:04 +0200

    features : add documentation
    on function precision and recall

--- izza/evaluation/evaluation.py
+++ izza/evaluation/evaluation.py
@@ -2,16 +2,66 @@ import numpy as np
 from sklearn.metrics import recall_score, precision_score
 
 
-def score_precision(i,scores,y_test):
-    "Assses the value of the precision at the percentage i"
-    ind = np.argpartition(scores, -int((i/100)*len(y_test)))[-int((i/100)*len(y_test)):]
-    pred = np.zeros(len(y_test))
-    pred[ind] = 1
-    return precision_score(y_test, pred)
-    
-def score_recall(i,scores,y_test):
-    "Assses the value of the recall at the percentage i"
-    ind = np.argpartition(scores, -int((i/100)*len(y_test)))[-int((i/100)*len(y_test)):]
-    pred = np.zeros(len(y_test))
-    pred[ind] = 1
-    return recall_score(y_test, pred)
+def fun_precision(pourcentage, predicted_proba, X, y):
+    '''
+    evaluate the precision on the dataset X
+
+    Parameters
+    ----------
+
+    pourcentage : the percentage of the population to take into account when
+        calculating the precision and the recall
+
+    predicted_proba : the probability of being a fraudster for each observation
+
+    X : the target variable
+        numpy array
+
+    y : the explaining features
+        numpy array
+
+    Return
+    -------
+
+    precision_score : the precision on the dataset
+
+    '''
+
+    ratio = -pourcentage * int(X.shape[0] / 100)
+    ind = np.argpartition(predicted_proba, ratio)[ratio:]
+    predicted = np.zeros(y.shape[0])
+    predicted[ind] = 1
+    return precision_score(y_true=y, y_pred=predicted)
+
+
+def fun_recall(pourcentage, predicted_proba, X, y):
+    '''
+    evaluate the recall on the dataset X
+
+    Parameters
+    ----------
+
+    pourcentage : the percentage of the population to take into account
+        when calculating the precision and the recall
+
+    predicted_proba : the probability of being a fraudster
+        for each observation
+
+    X : the target variable
+        numpy array
+
+    y : the explaining features
+        numpy array
+
+    Return
+    -------
+
+    recall_score : the recall on the dataset
+
+    '''
+
+    ratio = -pourcentage * int(X.shape[0] / 100)
+    ind = np.argpartition(predicted_proba, ratio)[ratio:]
+    predicted = np.zeros(y.shape[0])
+    predicted[ind] = 1
+    return recall_score(y_true=y, y_pred=predicted)
