2a53a8a56c9e4b428d76301f77d0fe8901bf764d
Ismaël <ismail.lachheb@octo.com>
2020-02-21 21:43:40 +0100

    centralize version from version.py file

--- setup.py
+++ setup.py
@@ -3,15 +3,21 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("izza/version.py") as fh:
+    version_dict = {}
+    exec(fh.read(), version_dict)
+    version = version_dict['__version__']
+
+
 setuptools.setup(
     name="izza",
-    version="0.1.1",
+    version=version,
     author="Ismaël Lachheb",
     author_email="ismael.lachheb@protonmail.com",
     description="A Personnal machine learning toolbox",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/lachhebo/izza", 
+    url="https://github.com/lachhebo/izza",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
